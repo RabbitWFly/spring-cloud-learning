6e5124b04745221df5e7133f75d571fb51ebc152
chentao <chentao@daokoudai.com>
2019-03-09 23:33:45 +0800

    add eureka

---
+++ rabbit-eureka/src/main/java/com/rabbit/eureka/RabbitEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.rabbit.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RabbitEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitEurekaApplication.class, args);
+    }
+
+}
