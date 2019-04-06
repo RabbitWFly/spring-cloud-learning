b4e045ab9ea287697898ba9d02bad4dbdb2c90d4
chentao <chentao@daokoudai.com>
2019-04-06 16:23:20 +0800

    服务调用认证

---
+++ rabbit-api-client/src/main/java/com/rabbit/api/client/auth/AuthRemoteClientHystrix.java
@@ -0,0 +1,10 @@
+package com.rabbit.api.client.auth;
+
+/**
+ * @Author chentao
+ * Date 2019/4/6
+ * Description
+ **/
+public class AuthRemoteClientHystrix {
+}
+
---
+++ rabbit-fsh-api/src/main/java/com/rabbit/fsh/api/filter/AuthHeaderFilter.java
@@ -0,0 +1,10 @@
+package com.rabbit.fsh.api.filter;
+
+/**
+ * @Author chentao
+ * Date 2019/4/6
+ * Description
+ **/
+public class AuthHeaderFilter {
+}
+
